c260cd5e9c37b2c4aa87f2d461df68a05f996829
Nikhi Katiyar <nikhilkatiyar0507@gmail.com>
2024-07-14 17:41:49 +0530

    02_operators

---
+++ 02_operators.js
@@ -0,0 +1,97 @@
+// activity 1 Airthmatic operation
+
+//task 1 to add two numbers
+
+
+// let num1=12
+// let num2=12
+// let num3=num1+num2
+// console.log(num3);
+
+//task2
+
+// let num1=15
+// let num2=12
+// let num3=num1-num2
+// console.log(num3);
+
+
+
+
+//task3
+
+// let num1=15
+// let num2=12
+// let num3=num1*num2
+// console.log(num3);
+
+
+//task4
+// let num1=15
+// let num2=12
+// let num3=num1/num2
+// console.log(num3);
+
+//task5
+
+// let num1=15
+// let num2=12
+// let num3=num1%num2
+// console.log(num3);
+
+
+
+//activity2   assignment operator
+
+
+// //task6
+
+// let number = 10; // Initial value of the variable
+// number += 5;     // Add 5 to the variable using the += operator
+// console.log(number); // Log the result to the console
+
+
+//task7
+
+// let number = 10; // Initial value of the variable
+// number -= 5;     // Add 5 to the variable using the -= operator
+// console.log(number); // Log the result to the console
+
+
+//activity 3 comparision operators
+
+//task8
+
+// console.log(7>8);
+// console.log(7>=7);
+// console.log(7>=7);
+// console.log(7==7);
+// console.log("7"===7);
+
+
+//activity4  logical operator
+
+// let num1=true
+// let num2=false
+// console.log(num1&&num2);
+
+// let num1=true
+// let num2=false
+// console.log(num1||num2);
+
+
+//activity5
+let number = -5; // You can change this value to test with different numbers
+let result = number >= 0 ? "The number is positive." : "The number is negative.";
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
