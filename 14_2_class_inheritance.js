055eaa256a6ea04d595cc92a3cc0aa271c635e56
Nikhi Katiyar <nikhilkatiyar0507@gmail.com>
2024-08-01 12:06:00 +0530

    day 14 inheritance done

---
+++ 14_2_class_inheritance.js
@@ -0,0 +1,32 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting() {
+        return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+    }
+
+    updateAge(newAge) {
+        this.age = newAge;
+        console.log(`Updated age is: ${this.age}`);
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, studentId) {
+        super(name, age);
+        this.studentId = studentId;
+    }
+
+    getStudentId() {
+        return `Student ID is: ${this.studentId}`;
+    }
+}
+
+// Create an instance of the Student class
+const studentInstance = new Student("Bob", 20, "S12345");
+
+// Log the student ID
+console.log(studentInstance.getStudentId());
