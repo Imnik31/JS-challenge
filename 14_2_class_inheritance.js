432b413fc44ca0fe93f522350b8d50bd0d6206a5
Nikhi Katiyar <nikhilkatiyar0507@gmail.com>
2024-08-01 12:13:09 +0530

    day 14 new code added

--- 14_2_class_inheritance.js
+++ 14_2_class_inheritance.js
@@ -1,3 +1,35 @@
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     greeting() {
+//         return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+//     }
+
+//     updateAge(newAge) {
+//         this.age = newAge;
+//         console.log(`Updated age is: ${this.age}`);
+//     }
+// }
+
+// class Student extends Person {
+//     constructor(name, age, studentId) {
+//         super(name, age);
+//         this.studentId = studentId;
+//     }
+
+//     getStudentId() {
+//         return `Student ID is: ${this.studentId}`;
+//     }
+// }
+
+// // Create an instance of the Student class
+// const studentInstance = new Student("Bob", 20, "S12345");
+
+// // Log the student ID
+// console.log(studentInstance.getStudentId());
 class Person {
     constructor(name, age) {
         this.name = name;
@@ -20,6 +52,10 @@ class Student extends Person {
         this.studentId = studentId;
     }
 
+    greeting() {
+        return `Hello, my name is ${this.name}, I am ${this.age} years old, and my student ID is ${this.studentId}.`;
+    }
+
     getStudentId() {
         return `Student ID is: ${this.studentId}`;
     }
@@ -28,5 +64,5 @@ class Student extends Person {
 // Create an instance of the Student class
 const studentInstance = new Student("Bob", 20, "S12345");
 
-// Log the student ID
-console.log(studentInstance.getStudentId());
+// Log the overridden greeting message
+console.log(studentInstance.greeting());
